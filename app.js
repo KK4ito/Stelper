9ca4bc5e4e8345bf04e46bd0eb3cbfb0095fcd77
Emil Sturzenegger <emilsblackberry@gmail.com>
2016-05-08 03:07:36 +0200

    error handling is now possible.

--- app.js
+++ app.js
@@ -38,7 +38,19 @@ angular.module('app').config(function($httpProvider, $urlRouterProvider, $stateP
     });
 });
 
-angular.module('app').run(function($rootScope, $state, $stateParams) {
+angular.module('app').run(function($rootScope) {
+
+    /*
+    Feature for adding alerts. Can't be done by service/factory
+    as those are singeltons.
+     */
+    $rootScope.alerts = [];
+    $rootScope.addAlert = function(type, msg) {
+        $rootScope.alerts.push({type:type, msg: msg});
+    };
+    $rootScope.closeAlert = function(index) {
+        $rootScope.alerts.splice(index, 1);
+    };
 
     /*
     For custom functions or pure javascript functions,
